2edcf1e75281abd017759b650c9cd88ce10ae6ed
Damien <abbemad@gmail.com>
2017-04-07 04:08:52 +0200

    With babel

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,10 @@
-var express = require ('express');
-var path = require ('path');
-var open = require ('open');
+import express from 'express'; 
+import path from 'path';
+import open from'open';
 
-var port = 3000;
-var app = express();
+const port = 3000;
+const app = express();
+// now when you put babel in front of your scripts in package.json you are enable to use ES6code in your files
 
 app.get('/', function (req, res){
     res.sendFile(path.join(__dirname, '../src/index.html'));
